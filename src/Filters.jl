65acf05c21680eefcfb006512824dd3e9a1fdf82
Sacnorth <sacnorth@gmail.com>
2016-02-22 19:37:12 +0530

    modified:   src/Filters.jl added createfirfilter with immutable

--- src/Filters.jl
+++ src/Filters.jl
@@ -2,10 +2,16 @@ module Filters
 
 export createfirfilt
 
+immutable FirFilter
+	filtobjPtr::Ptr{Void}
+end
+
 function createfirfilt(h::Array{Complex64})
 	ms = ccall((:firfilt_crcf_create, "libliquid"), Ptr{Void}, (Ptr{Complex64},Int), h,length(h));
-	return ms;
+	return FirFilter(ms);
 
 end
+
+function recreatefirfilter()
 
 end
