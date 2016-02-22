de0fa377c532b0403d2f09c20e7421fcdb429ea7
Sacnorth <sacnorth@gmail.com>
2016-02-22 19:22:46 +0530

    modified:   src/Filters.jl  Added firfilt create function

--- src/Filters.jl
+++ src/Filters.jl
@@ -1,4 +1,9 @@
 module Filters
 
+function createfirfilt(h::Array{Complex64})
+	ms = ccall((:firfilt_crcf_create, "libliquid"), Ptr{Void}, (Ptr{Complex64},Int), h,length(h));
+	return ms;
+
+end
 
 end
