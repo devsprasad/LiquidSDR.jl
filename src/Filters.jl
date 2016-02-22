c80072453dcacfc90759cbcf2f2b72747d7535c5
Sprasad <devsprasadbhat@gmail.com>
2016-02-22 19:26:24 +0530

    added export for createfirfilt function

--- src/Filters.jl
+++ src/Filters.jl
@@ -1,5 +1,7 @@
 module Filters
 
+export createfirfilt
+
 function createfirfilt(h::Array{Complex64})
 	ms = ccall((:firfilt_crcf_create, "libliquid"), Ptr{Void}, (Ptr{Complex64},Int), h,length(h));
 	return ms;
